8ad526edead8487fb4d126d3347d7281efbeef46
bcoles <bcoles@gmail.com>
2025-02-27 22:27:36 +1100

    spec: Add missing tests (#10)
    * spec/arch: Add missing endian tests
    
    * spec/arch: Add missing pack_addr tests
    
    * spec/arch/x86: Add tests for register_names_to_ids method

--- spec/lib/rex/arch_spec.rb
+++ spec/lib/rex/arch_spec.rb
@@ -118,6 +118,14 @@ RSpec.describe Rex::Arch do
       end
     end
 
+    context "when arch is ARCH_PPCE500V2" do
+      let(:arch) { Rex::Arch::ARCH_PPCE500V2 }
+      let(:addr) { 0x41424344 }
+      it "packs addr as 32-bit unsigned, big-endian" do
+        is_expected.to eq("ABCD")
+      end
+    end
+
     context "when arch is ARCH_SPARC" do
       let(:arch) { Rex::Arch::ARCH_SPARC }
       let(:addr) { 0x41424344 }
@@ -230,13 +238,17 @@ RSpec.describe Rex::Arch do
       {
         Rex::Arch::ARCH_X86 => Rex::Arch::ENDIAN_LITTLE,
         Rex::Arch::ARCH_X86_64 => Rex::Arch::ENDIAN_LITTLE,
+        Rex::Arch::ARCH_X64 => Rex::Arch::ENDIAN_LITTLE,
         Rex::Arch::ARCH_MIPS => Rex::Arch::ENDIAN_BIG,
         Rex::Arch::ARCH_MIPSLE => Rex::Arch::ENDIAN_LITTLE,
         Rex::Arch::ARCH_MIPSBE => Rex::Arch::ENDIAN_BIG,
         Rex::Arch::ARCH_MIPS64 => Rex::Arch::ENDIAN_BIG,
+        Rex::Arch::ARCH_MIPS64LE => Rex::Arch::ENDIAN_LITTLE,
         Rex::Arch::ARCH_PPC => Rex::Arch::ENDIAN_BIG,
         Rex::Arch::ARCH_PPC64LE => Rex::Arch::ENDIAN_LITTLE,
+        Rex::Arch::ARCH_PPCE500V2 => Rex::Arch::ENDIAN_BIG,
         Rex::Arch::ARCH_SPARC => Rex::Arch::ENDIAN_BIG,
+        Rex::Arch::ARCH_SPARC64 => Rex::Arch::ENDIAN_BIG,
         Rex::Arch::ARCH_ARMLE => Rex::Arch::ENDIAN_LITTLE,
         Rex::Arch::ARCH_ARMBE => Rex::Arch::ENDIAN_BIG,
         Rex::Arch::ARCH_AARCH64 => Rex::Arch::ENDIAN_LITTLE,
@@ -245,6 +257,7 @@ RSpec.describe Rex::Arch do
         Rex::Arch::ARCH_RISCV64BE => Rex::Arch::ENDIAN_BIG,
         Rex::Arch::ARCH_RISCV64LE => Rex::Arch::ENDIAN_LITTLE,
         Rex::Arch::ARCH_LOONGARCH64 => Rex::Arch::ENDIAN_LITTLE,
+        Rex::Arch::ARCH_ZARCH => Rex::Arch::ENDIAN_BIG,
       }
     end
     subject { described_class.endian(arch) }
